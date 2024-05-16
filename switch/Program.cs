6bd8ef26cc467b44747de9baa4fd0c4b1b1ae55d
D11rr <145238391+D11rr@users.noreply.github.com>
2024-05-16 11:09:06 +0300

    Add files via upload

---
+++ switch/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace switchi;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("INK");
+        Console.WriteLine("kirjuta 1 et valida nimekirjast, 2 et jah");
+        string valik = Console.ReadLine();
+        if (valik == "1")
+        {
+            hee();
+        }
+        else if (valik == "2")
+        {
+            Console.WriteLine("SIIN POLE NIMEKIRJA");
+        }
+       
+
+        
+    }
+    static void hee()
+    {
+Console.WriteLine("vali 1-3 et saaada nimi");
+        string numba = Console.ReadLine();
+        
+        switch (numba)
+        {
+            case "1":
+                Console.WriteLine("Toivo");
+                break;
+            case "2":
+                Console.WriteLine("Uibo");
+                break;
+            case "3":
+                Console.WriteLine("Jarmo");
+                break;
+            default:
+                Console.WriteLine("Sa ei valinud õiget numbrit. Saad Kaja Kallas");
+                break;
+        }
+    }
+}
